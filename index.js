7c25c2b7ffa298a12c3fb31b8ed74eb0d3050403
winter <jsylego@naver.com>
2020-12-11 21:45:01 +0900

    포트 테스트(80)

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use('/posts', require('./routes/posts'));
 app.use('/users', require('./routes/users'));
 
 // Port setting
-var port = 3000;
+var port = 80;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
