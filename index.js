63bb06ab1686f971087c98937481c6303faa789d
winter <jsylego@naver.com>
2020-12-11 21:51:59 +0900

    heroku deploy를 위한 포트 설정

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use('/posts', require('./routes/posts'));
 app.use('/users', require('./routes/users'));
 
 // Port setting
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
